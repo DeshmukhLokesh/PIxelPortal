05c921622a970dc9cfe21894316a9a1c13e1587a
DeshmukhLokesh <lokesh@internationallinx.com>
2014-07-17 18:50:02 +0530

    Initial Draft
    Initial Draft

---
+++ build/production/MyApp/resources/Readme.md
@@ -0,0 +1,3 @@
+# ext-theme-classic-6f8470cf-d898-42d4-9aca-fe9c608b69e2/resources
+
+This folder contains static resources (typically an `"images"` folder as well).
---
+++ build/temp/production/MyApp/sencha-compiler/cmd-packages.js
@@ -0,0 +1,4 @@
+Ext.setVersion("ext-theme-base", "4.2.1");
+Ext.setVersion("ext-theme-classic", "4.2.1");
+Ext.setVersion("ext-theme-classic-6f8470cf-d898-42d4-9aca-fe9c608b69e2", "1.0.0");
+Ext.setVersion("ext-theme-neutral", "4.2.1");
